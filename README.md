3dfe54fdc9ad0c3c607c23be8ef490dab2d26d9e
pía peña <mariapiapenafoissac@gmail.com>
2020-10-19 11:06:18 -0300

    beta 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # forgot-n-light
 here are one software can detect light if you left your desctop. 
+created in arduino by: María pía peña foissac.
---
+++ forgotnlight/forgotnlight.ino
@@ -0,0 +1,14 @@
+#define light (2) // camera detecting light 
+#define i'm  //detect person front of desctop (not tone)
+
+void setup()
+{
+
+}
+
+void loop()
+{
+if (light==1, i'm == 0);{}
+tone(4, 48000);
+
+}
